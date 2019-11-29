4bfe40f68de81da63842333b299c4e45c6fe629f
1styrGroupB <1styrGroupB@pn.local>
2019-11-29 21:53:04 +0800

    login_practice

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -16,13 +16,13 @@
           >
             <v-card class="elevation-12">
               <v-toolbar
-                color="primary"
+                color="success"
                 dark
                 flat
               >
-                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-toolbar-title>My Inventory</v-toolbar-title>
                 <v-spacer />
-                <v-tooltip bottom>
+                <!-- <v-tooltip bottom>
                   <template v-slot:activator="{ on }">
                     <v-btn
                       :href="source"
@@ -35,8 +35,8 @@
                     </v-btn>
                   </template>
                   <span>Source</span>
-                </v-tooltip>
-                <v-tooltip right>
+                </v-tooltip> -->
+                <!-- <v-tooltip right>
                   <template v-slot:activator="{ on }">
                     <v-btn
                       icon
@@ -49,15 +49,18 @@
                     </v-btn>
                   </template>
                   <span>Codepen</span>
-                </v-tooltip>
+                </v-tooltip> -->
               </v-toolbar>
               <v-card-text>
-                <v-form>
+                <v-form @submit.prevent="validate">
                   <v-text-field
                     label="Login"
                     name="login"
                     prepend-icon="mdi-account"
                     type="text"
+                    v-model="username"
+                    required
+                    v-bind:class="{mierror: usererr}" class="myinput mi"
                   />
 
                   <v-text-field
@@ -65,14 +68,21 @@
                     label="Password"
                     name="password"
                     prepend-icon="mdi-lock"
-                    type="password"
+                    type="text"
+                    v-model="password"
+                    required 
+                    v-bind:class="{mierror: passerr}" class="myinput mi"
                   />
                 </v-form>
               </v-card-text>
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="primary">Login</v-btn>
+                <v-btn color="success btn-xl" type="submit">Login</v-btn>
               </v-card-actions>
+              <div v-show="hasError" class="error">
+            <span v-if="usererr" class="er">Incorrect username!</span>
+            <span v-if="passerr" class="er">Incorrect password!</span>
+        </div>
             </v-card>
           </v-col>
         </v-row>
@@ -81,10 +91,83 @@
   </v-app>
 </template>
 
-<script>
-  export default {
-    props: {
-      source: String,
-    },
+<script lang="ts">
+//   export default {
+//     props: {
+//       source: String,
+//     },
+//   }
+// </script>
+
+// <script lang="ts">
+export default {
+  name: "Login",
+  data() {
+    return {
+        username: "",
+        password: "",
+        passerr: false,
+        usererr: false,
+        hasError: false,
+    };
+  }, methods: {
+      validate() {
+          
+          this.usererr = this.username != "chan";
+          this.passerr = this.password != "chan";
+            if (!this.usererr && !this.passerr) {
+                this.hasError = false;
+            } else {
+                this.hasError = true;
+            }
+          if (!this.passerr && !this.usererr) {
+              this.$router.push("/dashboard");
+          }
+      }
   }
+};
 </script>
+<style>
+
+/* .lcont { */
+  /* height: 100vh; */
+  /* text-align: center; */
+  /* background-image: url("../assets/back.jpg"); */
+/* background-size: cover; */
+  /* background-image: linear-gradient(to bottom right, white, rgb(199, 222, 247)); */
+/* } */
+/* .fb {
+  background:white;
+  margin:0;
+  padding:1px;
+} */
+/* .loginholder { */
+  /* border-radius: 1px; */
+  /* display: inline-block; */
+  /* border: 2px solid #6ec2f3; */
+  /* margin-top: 120px; */
+  /* background: white; */
+/* } */
+/* .mi {
+  margin: 27px;
+  width: 230px;
+} */
+.error {
+  color: rgb(243, 0, 0);
+  background: rgb(250, 219, 219);
+}
+/* .bi {
+  margin-top: 0px !important;
+  margin-bottom: 27px !important;
+  display: inline-block;
+}
+.lh {
+  background:rgba(198, 226, 248, 0.7);
+  padding: 15px;
+  margin:0;
+}
+.er {
+    display:block;
+    /* margin: 5px; */
+/* } */ 
+</style>
---
+++ src/views/Login1.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="lcont">
+    <!-- <div class="home"></div> -->
+    <div class="loginholder">
+      <div class="lh">
+        <span class="comm">COMM</span>
+        <span class="unity">UNITY</span>
+      </div>
+      <div class="fb">
+      <form @submit.prevent="validate">
+        <input type="text" placeholder="Username" v-model="username" required v-bind:class="{mierror: usererr}" class="myinput mi" />
+
+        <input type="text" placeholder="Password" v-model="password" required v-bind:class="{mierror: passerr}" class="myinput mi" />
+        <button class="mybtn bi" type="submit">Login</button>
+        <div v-show="hasError" class="error">
+            <span v-if="usererr" class="er">Incorrect username!</span>
+            <span v-if="passerr" class="er">Incorrect password!</span>
+        </div>
+      </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Login",
+  data() {
+    return {
+        username: "",
+        password: "",
+        passerr: false,
+        usererr: false,
+        hasError: false,
+    };
+  }, methods: {
+      validate() {          
+          this.usererr = this.username != "rivas";
+          this.passerr = this.password != "rivas";
+            if (!this.usererr && !this.passerr) {
+                this.hasError = false;
+            } else {
+                this.hasError = true;
+            }
+          if (!this.passerr && !this.usererr) {
+              this.$router.push("/dashboard");
+          }
+      }
+  }
+};
+</script>
+<style>
+.lcont {
+  height: 100vh;
+  text-align: center;
+  /* background-image: url("../assets/back.jpg"); */
+background-size: cover;
+  /* background-image: linear-gradient(to bottom right, white, rgb(199, 222, 247)); */
+}
+.fb {
+  background:white;
+  margin:0;
+  padding:1px;
+}
+.loginholder {
+  border-radius: 1px;
+  display: inline-block;
+  /* border: 2px solid #6ec2f3; */
+  margin-top: 120px;
+  /* background: white; */
+}
+.mi {
+  margin: 27px;
+  width: 230px;
+}
+.error {
+  color: rgb(243, 0, 0);
+  background: rgb(250, 219, 219);
+}
+.bi {
+  margin-top: 0px !important;
+  margin-bottom: 27px !important;
+  display: inline-block;
+}
+.lh {
+  background:rgba(198, 226, 248, 0.7);
+  padding: 15px;
+  margin:0;
+}
+.er {
+    display:block;
+    /* margin: 5px; */
+}
+</style>
