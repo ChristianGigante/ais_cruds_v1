fc0541d7815583771d8558a1ee31782930cbd98e
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-12-01 16:57:49 +0800

    final ui

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,8 @@
     :headers="headers"
     :items="desserts"
     sort-by="calories"
-    class="elevation-1"
+    class="elevation-1 container"
+    :search="search"
   >
     <template v-slot:top>
       <v-toolbar flat color="white">
@@ -13,7 +14,17 @@
           inset
           vertical
         ></v-divider>
+        <!-- search field -->
+        <v-text-field
+        v-model="search"
+        prepend-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+        autofocus
+      ></v-text-field>
         <v-spacer></v-spacer>
+        <!-- dialog card -->
         <v-dialog v-model="dialog" max-width="500px" >
           <template v-slot:activator="{ on }">
             <v-btn color="primary" outlined  dark class="mb-2" v-on="on">New Item</v-btn>
@@ -27,28 +38,28 @@
               <v-container>
                 <v-row>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                    <v-text-field v-model="editedItem.name" label="Item Name" required></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                    <v-text-field v-model="editedItem.calories" label="Quantity" required type="number" min="1" ></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
-                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                    <v-text-field v-model="editedItem.fat" label="Priority" required type="number" min="1" max="3"></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6" md="4">
+                  <!-- <v-col cols="12" sm="6" md="4">
                     <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
                   </v-col>
                   <v-col cols="12" sm="6" md="4">
                     <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                  </v-col>
+                  </v-col> -->
                 </v-row>
               </v-container>
             </v-card-text>
 
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+              <v-btn color="blue darken-1" outlined text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" outlined text @click="save">Save</v-btn>
             </v-card-actions>
           </v-card>
         </v-dialog>
@@ -78,36 +89,36 @@
   export default {
     data(){
       return {
-        search:"",
+      search:"",
       dialog: false,
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Item Name',
           align: 'left',
-          sortable: false,
+          sortable: true,
           value: 'name',
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Quantity', value: 'quantity' },
+        { text: 'Priority', value: 'priority' },
+        // { text: 'Carbs (g)', value: 'carbs' },
+        // { text: 'Protein (g)', value: 'protein' },
         { text: 'Actions', value: 'action', sortable: false },
       ],
       desserts: [],
       editedIndex: -1,
       editedItem: {
         name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        quantity: 0,
+        priority: 0,
+        // carbs: 0,
+        // protein: 0,
       },
       defaultItem: {
         name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0,
+        quantity: 0,
+        priority: 0,
+        // carbs: 0,
+        // protein: 0,
       },
       }
     },
@@ -133,73 +144,73 @@
         this.desserts = [
           {
             name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
+            quantity: 159,
+            priority: 6.0,
+            // carbs: 24,
+            // protein: 4.0,
           },
           {
             name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
+            quantity: 237,
+            priority: 9.0,
+            // carbs: 37,
+            // protein: 4.3,
           },
           {
             name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
+            quantity: 262,
+            priority: 16.0,
+            // carbs: 23,
+            // protein: 6.0,
           },
           {
             name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
+            quantity: 305,
+            priority: 3.7,
+            // carbs: 67,
+            // protein: 4.3,
           },
           {
             name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
+            quantity: 356,
+            priority: 16.0,
+            // carbs: 49,
+            // protein: 3.9,
           },
           {
             name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
+            quantity: 375,
+            priority: 0.0,
+            // carbs: 94,
+            // protein: 0.0,
           },
           {
             name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
+            quantity: 392,
+            priority: 0.2,
+            // carbs: 98,
+            // protein: 0,
           },
           {
             name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
+            quantity: 408,
+            priority: 3.2,
+            // carbs: 87,
+            // protein: 6.5,
           },
           {
             name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
+            quantity: 452,
+            priority: 25.0,
+            // carbs: 51,
+            // protein: 4.9,
           },
           {
             name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
+            quantity: 518,
+            priority: 26.0,
+            // carbs: 65,
+            // protein: 7,
           },
         ]
       },
