fde768b5ab5228d5f8c8cd8f28c06bf2fb098aa7
Christian Gigante <christian.gigante@student.passerellesnumeriques.org>
2019-12-03 18:37:17 +0800

    update

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -169,81 +169,6 @@ export default {
         }
       });
     },
-    initialize() {
-      this.desserts = [
-        // {
-        //   name: "Frozen Yogurt",
-        //   quantity: 159,
-        //   priority: 6.0
-        //   // carbs: 24,
-        //   // protein: 4.0,
-        // },
-        // {
-        //   name: "Ice cream sandwich",
-        //   quantity: 237,
-        //   priority: 9.0
-        //   // carbs: 37,
-        //   // protein: 4.3,
-        // },
-        // {
-        //   name: "Eclair",
-        //   quantity: 262,
-        //   priority: 16.0
-        //   // carbs: 23,
-        //   // protein: 6.0,
-        // },
-        // {
-        //   name: "Cupcake",
-        //   quantity: 305,
-        //   priority: 3.7
-        //   // carbs: 67,
-        //   // protein: 4.3,
-        // },
-        // {
-        //   name: "Gingerbread",
-        //   quantity: 356,
-        //   priority: 16.0
-        //   // carbs: 49,
-        //   // protein: 3.9,
-        // },
-        // {
-        //   name: "Jelly bean",
-        //   quantity: 375,
-        //   priority: 0.0
-        //   // carbs: 94,
-        //   // protein: 0.0,
-        // },
-        // {
-        //   name: "Lollipop",
-        //   quantity: 392,
-        //   priority: 0.2
-        //   // carbs: 98,
-        //   // protein: 0,
-        // },
-        // {
-        //   name: "Honeycomb",
-        //   quantity: 408,
-        //   priority: 3.2
-        //   // carbs: 87,
-        //   // protein: 6.5,
-        // },
-        // {
-        //   name: "Donut",
-        //   quantity: 452,
-        //   priority: 25.0
-        //   // carbs: 51,
-        //   // protein: 4.9,
-        // },
-        // {
-        //   name: "KitKat",
-        //   quantity: 518,
-        //   priority: 26.0
-        //   // carbs: 65,
-        //   // protein: 7,
-        // }
-      ];
-    },
-
     editItem(item) {
       this.editedIndex = this.desserts.indexOf(item);
       this.editedItem = Object.assign({}, item);
@@ -252,14 +177,12 @@ export default {
 
     deleteItem(item) {
       const index = this.desserts.indexOf(item);
-      // confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
       let ans = confirm("Are you sure you want to delete this item?");
       if (ans) {
         axios
           .delete("http://localhost:3000/ais/deleteItem/" + item)
           .then( () => {
             alert("Successfully Deleted!")
-            // this.desserts.splice(index, 1); //delete Item
           });
         this.desserts.splice(index, 1)
       }
